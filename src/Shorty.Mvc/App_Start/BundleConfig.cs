e6ca5aada637f2aa16c1954c7a117776707adb6b
Abdul Munim <iam@munim.net>
2015-11-30 13:02:42 +0000

    Removed dependency nuget for front-end.

--- src/Shorty.Mvc/App_Start/BundleConfig.cs
+++ src/Shorty.Mvc/App_Start/BundleConfig.cs
@@ -8,24 +8,27 @@ namespace Shorty.Mvc
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            var scripts = new ScriptBundle("~/bundles/scripts").Include(
+                "~/bower_components/jquery/dist/jquery.min.js",
+                "~/bower_components/jquery-validate/dist/jquery.validate.min.js",
+                "~/bower_components/bootstrap/dist/js/bootstrap.min.js"
+                );
+            scripts.Orderer = new AsIsBundleOrderer();
+            bundles.Add(scripts);
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/bower_components/modernizr-min/dist/modernizr.min.js",
+                        "~/bower_components/respond/dest/respond.min.js"));
 
-            #region Foundation Bundles
+            var styles = new StyleBundle("~/bundles/css").Include(
+                "~/bower_components/bootstrap/dist/css/bootstrap.min.css",
+                "~/bower_components/bootstrap/dist/css/bootstrap-theme.min.css",
+                "~/Content/Site.css");
+            styles.Orderer = new AsIsBundleOrderer();
+            bundles.Add(styles);
 
-            bundles.Add(Foundation.Styles());
-
-            bundles.Add(Foundation.Scripts());
-
-            #endregion
         }
     }
 }
