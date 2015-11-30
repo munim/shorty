87b251d9dcc3a9077baa4a4f979acaa687ebc305
Abdul Munim <iam@munim.net>
2015-11-30 13:04:10 +0000

    Added unsorted ordering.

---
+++ src/Shorty.Mvc/App_Start/AsIsBundleOrderer.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Web.Optimization;
+
+namespace Shorty.Mvc
+{
+    public class AsIsBundleOrderer : IBundleOrderer
+    {
+        public IEnumerable<BundleFile> OrderFiles(BundleContext context, IEnumerable<BundleFile> files)
+        {
+            return files;
+        }
+    }
+}
